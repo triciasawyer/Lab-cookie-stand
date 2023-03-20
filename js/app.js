bd038f8e9d27989464acafbc41a1a853b611191a
Tricia Sawyer <tricia.sawyer03@gmail>
2023-03-20 18:04:57 -0500

    add loops and fix things

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
 'use strict';
 console.log('js connected');
 
-let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 console.log('🚀 ~ file: app.js:5 ~ hours:', hours);
 
 let storeOne = {
   storeName: 'Seattle',
-  minCust: '23',
-  maxCust: '65',
-  avgCust: '6.3',
-  setCustomer: function () {
-    for (let i = 0; i < hours.length; i++) {
-      this.Customer = randomCustomer(23, 65) + ' Customers per hour.';
+  hourlyTotal: [],
+  minCust: 23,
+  maxCust: 65,
+  avgCookies: 6.3,
+  avgCustomer: 0,
+  cookiesPerHour: [],
+  setCookies:function(){
+    for (let i = 0; i < hours.length; i++){
+      this.cookiesPerHour[i] = hours[i] + ' ' + Math.floor(this.avgCookies * randomCustomer(this.minCust, this.maxCust));
     }
-  }
+  },
 };
 
-function randomCustomer(minCust, maxCust) {
+
+function randomCustomer(minCust, maxCust){
   console.log('🚀 ~ file: app.js:18 ~ randomCustomer ~ randomCustomer:', randomCustomer);
   return Math.floor(Math.random() * (maxCust - minCust) + minCust);
 }
@@ -34,30 +38,34 @@ function randomCustomer(minCust, maxCust) {
 
 //let storeTwo = {
 //   storeName: 'Tokyo',
-//    minCust: '3',
-//    maxCust: '24',
-//   avgCust: '1.2',
+//   hourlyTotal: [],
+//   minCust: 3,
+//   maxCust: 24,
+//   avgCookies: 1.2,
 //}
 
 //let storeThree = {
-//    storeName: 'Dubai',
-//   minCust: '11',
-//   maxCust: '38',
-//    avgCust: '3.7',
+//   storeName: 'Dubai',
+//   hourlyTotal: [],
+//   minCust: 11,
+//   maxCust: 38,
+//   avgCookies: 3.7,
 //}
 
 //let storeFour = {
 //    storeName: 'Paris',
-//    minCust: '20',
-//    maxCust: '38',
-//    avgCust: '2.3',
+//    hourlyTotal: [],
+//    minCust: 20,
+//    maxCust: 38,
+//    avgCookies: 2.3,
 //}
 
 //let storeFive = {
 //   storeName: 'Lima',
-//   minCust: '2',
-//   maxCust: '16',
-//   avgCust: '4.6',
+//   hourlyTotal: [],
+//   minCust: 2,
+//   maxCust: 16,
+//   avgCookies: 4.6,
 //}
 
 //helper function
@@ -66,8 +74,11 @@ function randomCustomer(minCust, maxCust) {
 
 //}
 
-storeOne.Customers;
-//storeTwo.Customers
-//storeThree.Customers
-//storeFour.Customers
-//storeFive.Customers
+storeOne.setCookies();
+console.log('🚀 ~ file: app.js:74 ~ storeOne:", storeOne');
+
+
+//storeTwo.
+//storeThree.
+//storeFour.
+//storeFive.
